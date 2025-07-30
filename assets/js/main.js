77a95de2a506c6656b856bc461e9a6169b316818
Luiz Messias <luizgdsmdev@gmail.com>
2025-07-30 11:00:07 -0300

    Adding life logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,12 +12,21 @@ const state = {
         totalScore: 0,
         gameTime: 0,
         gameLives: 3,
+        timeForChange: 2000,
+        ralphHitcontroller: true,
     }
 }
 
 let RandomSquareForRalph = () => Math.floor(Math.random() * 9);
 let clearSquareClassFocus = () => state.viwes.game__boardSquare.forEach(square =>{ square.classList.remove("game__board-focus"); });
-let MoveRalphInsertion = () => state.values.randomSquareId = setInterval(RalphInsertion, 2000);
+
+let intervalId = null;
+let MoveRalphInsertion = () => {
+    //Clear setInterval on click
+    intervalId !== null ? clearInterval(intervalId) : "";
+    RalphInsertion();
+    intervalId = setInterval(RalphInsertion, state.values.timeForChange);
+}
 
 let RandomSquareClassInsertion = () => {
     let randomSquare = RandomSquareForRalph();
@@ -33,9 +42,20 @@ let RandomSquareClassInsertion = () => {
     state.values.currentPosition = randomSquare;
 }   
 
+let RalphInsertionController = true;
 let RalphInsertion = () => {
     clearSquareClassFocus();
-    RandomSquareClassInsertion();
+
+    //Controlling the life cicle at each time Ralph change square
+    if(RalphInsertionController){
+        RandomSquareClassInsertion();
+        RalphInsertionController = false;
+    }else{
+        state.values.ralphHitcontroller === true ? lifeHandler() : "";
+        RandomSquareClassInsertion();
+        state.values.ralphHitcontroller = true;
+
+    }
 }
 
 let scoreHandler = (points) =>{
@@ -43,28 +63,39 @@ let scoreHandler = (points) =>{
     state.viwes.top__scoreCounter.innerText = state.values.totalScore;
 }
 
+let lifeHandler = () =>{
+    if(state.values.gameLives > 1){
+        state.values.gameLives -= 1;
+        state.viwes.lives__liveCounter.innerText = state.values.gameLives;
+    }else{
+        state.viwes.lives__liveCounter.innerText = '0';
+        alert("Game over");
+    }
+    
+}
 
-
-
-
+//Add logic on square click
 let AddListenerForSquare = () => {
     state.viwes.game__boardSquare.forEach((square) => {
         square.addEventListener("click", (event) =>{
             event.stopImmediatePropagation();
             
-            if(parseInt(square.id) === state.values.currentPosition){
+            //Add or subtract points from total score
+            //Controll Ralph's position on click
+            if(parseInt(square.id) === state.values.currentPosition ){
+                state.values.ralphHitcontroller = false;
+                MoveRalphInsertion();
                 scoreHandler(10);
             }else{
                 scoreHandler(-10);
             }
-            
+
         });
     });
 };
 
 function main(){
 AddListenerForSquare();
-RandomSquareForRalph();
 MoveRalphInsertion();
 }
 
